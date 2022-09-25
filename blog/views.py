119f8f3131adc92f296dfe38bab8e8fb5db74d3c
mogubudu <pipkin.vladislav@gmail.com>
2022-09-25 16:21:20 +0300

    optimize comments count evaluation

--- blog/views.py
+++ blog/views.py
@@ -46,17 +46,15 @@ def serialize_tag(tag):
 
 
 def index(request):
-    popular_posts = Post.objects.annotate(likes_count=Count('likes')).order_by('-likes_count').prefetch_related('author')
-    popular_posts_ids = [post.id for post in popular_posts]
+    most_popular_posts = Post.objects.annotate(likes_count=Count('likes')).order_by('-likes_count').prefetch_related('author')[:5]
+    most_popular_posts_ids = [post.id for post in most_popular_posts]
     
-    posts_with_comments = Post.objects.filter(id__in=popular_posts_ids).annotate(comments_count=Count('comments'))
+    posts_with_comments = Post.objects.filter(id__in=most_popular_posts_ids).annotate(comments_count=Count('comments'))
     ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
     count_for_id = dict(ids_and_comments)
 
-    for post in popular_posts:
+    for post in most_popular_posts:
         post.comments_count = count_for_id[post.id]
-    
-    most_popular_posts = popular_posts[:5]
 
     fresh_posts = Post.objects.annotate(comments_count=Count('comments')).prefetch_related('author').order_by('-published_at')
     most_fresh_posts = fresh_posts[:5]

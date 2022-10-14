c5efdd18a039d85ed8e9892ee451bcaf9deea983
Vladimir Shatskiy <vovaysach@gmail.com>
2022-10-15 01:53:07 +0300

    reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -12,7 +12,7 @@ def serialize_post(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag(tag) for tag in post.tags.annotate(num_posts=Count('posts'))],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -20,7 +20,7 @@ def serialize_post(post):
 def serialize_tag(tag):
     return {
         'title': tag.title,
-        'posts_with_tag': tag.posts.count(),
+        'posts_with_tag': tag.num_posts,
     }
 
 
@@ -30,8 +30,9 @@ def index(request):
                                      .prefetch_related('author', 'tags')[:5] \
                                      .fetch_with_comments_count()
 
-    fresh_posts = Post.objects.prefetch_related('author', 'tags').annotate(comments_count=Count('comments')) \
-                    .order_by('published_at')
+    fresh_posts = Post.objects.prefetch_related('author', 'tags') \
+                              .annotate(comments_count=Count('comments')) \
+                              .order_by('published_at')
     most_fresh_posts  = list(fresh_posts)[-5:]
 
     most_popular_tags = Tag.objects.popular()[:5]

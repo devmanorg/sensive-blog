cd13cf0556e5519a729c31f97a202f72da205911
kiril <kirill2689@gmail.com>
2024-01-20 10:59:22 +0300

    formatting

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from blog.models import Post, Tag, Comment
 
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ("author", "likes", "tags")
+
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     raw_id_fields = ("author", "post",)
 
-# admin.site.register(Post)
+
 admin.site.register(Tag)
-# admin.site.register(Comment)
